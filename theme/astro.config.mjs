a6cabd339ebdd05da1e21d7bcde33483554c2d7d
Rostislav Egorov <rpegorov@icloud.com>
2022-12-29 13:11:25 +0300

    config astro.deploy github pages

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -6,8 +6,9 @@ import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://odyssey-theme.littlesticks.dev/',
+  site: 'https://rpegorov.github.io',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  base: '/etalon-expert',
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), image(), lit()], // Add renderers to the config
