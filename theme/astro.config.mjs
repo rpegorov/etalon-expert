7737c129f37db6ae6d08e8a3f0128f103bbcab07
Rostislav Egorov <rpegorov@icloud.com>
2023-01-24 14:24:09 +0300

    added base from gitHub pages

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -7,9 +7,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://rpegorov.github.io',
+  site: 'https://rpegorov.github.io',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  // base: '/etalon-expert',
+  base: '/etalon-expert',
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), image(), lit(), react()], // Add renderers to the config
