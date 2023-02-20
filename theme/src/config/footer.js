993af2c7a06051610314bfbdfd4926486a60b2ac
Rostislav Egorov <rpegorov@icloud.com>
2023-02-20 09:51:13 +0300

    правки по тексту, опечатки

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -2,22 +2,22 @@ export const footerSocials = [
   {
     name: 'Telephone',
     url: 'tel:+79121926760',
-    icon: "mdi-telephone",
+    icon: "mdi:telephone",
   },
   {
     name: 'Email',
     url: 'mailto:etaloneo@ya.ru',
-    icon: "mdi-email",
+    icon: "mdi:email",
   },
   {
     name: 'WatsApp',
     url: 'https://wa.me/79121926760?text=Добрый день!',
-    icon: "mdi-whatsapp",
+    icon: "mdi:whatsapp",
   },
   {
     name: 'Telegram',
     url: 'tg://t.me/+79128633210',
-    icon: "mdi-telegram",
+    icon: "mdi:telegram",
   },
 ]
 
@@ -70,7 +70,7 @@ export const footerLists = [
 				slug: 'https://yandex.ru/maps/19/syktyvkar/?ll=50.836659%2C61.672776&mode=routes&rtext=~61.672776%2C50.836659&rtt=auto&ruri=~ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1NzU2NzUyNRJh0KDQvtGB0YHQuNGPLCDQoNC10YHQv9GD0LHQu9C40LrQsCDQmtC%2B0LzQuCwg0KHRi9C60YLRi9Cy0LrQsNGALCDQodC%2B0LLQtdGC0YHQutCw0Y8g0YPQu9C40YbQsCwgOCIKDb1YS0IV7LB2Qg%3D%3D&z=17.01',
 			},
 			{
-				title: '2gis',
+				title: '2GIS',
 				slug: 'https://2gis.ru/syktyvkar/directions/points/%7C50.836641%2C61.672854%3B70000001022397997?m=50.836641%2C61.672854%2F16"',
 			},
 		],
--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,8 +1,7 @@
 export default {
-	title: `ЭТАЛОН эксперт-оценка`,
+	title: `ЭТАЛОН Эксперт-Оценка`,
 	description: `Экспертно-оценочная организация работающая для Вашего комфорта`,
-	// url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `ЭТАЛОН`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	name: `Эталон Эксперт-Оценка`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
